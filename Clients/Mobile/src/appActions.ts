d5006d795252aa0696c9cdeb4e7de87e7a49f30e
Jon Kelling <jonkelling@Jons-MacBook-Air.local>
2017-05-06 23:58:56 -0500

    * Changed "thunk" actions to normal actions. * Adding meta/schema and meta/source to each CALL_API action type  (REQUEST, SUCCESS, FAILURE).

--- Clients/Mobile/src/appActions.ts
+++ Clients/Mobile/src/appActions.ts
@@ -9,50 +9,67 @@ const baseUrl = "http://localhost:5000/api/v1";
 export default {
     loadCurrentUser: () => {
         const endpoint = `${baseUrl}/users/me`;
-        return (dispatch) => dispatch(getCallApiAction(endpoint, schemas.user));
+        return getCallApiAction(endpoint, schemas.user);
     },
     loadCommunity: (communityId: number) => {
         const endpoint = `${baseUrl}/communities/${communityId}`;
-        return (dispatch) => dispatch(getCallApiAction(endpoint, schemas.community));
+        return getCallApiAction(endpoint, schemas.community);
     },
     loadNewestPosts: (communityId: number, limit = null) => {
         const now = new Date().toUTCString();
-        const endpoint = `${baseUrl}/communities/${communityId}/posts?before=${now}&limit=${limit || ""}`;
-        return (dispatch) => dispatch(getCallApiAction(endpoint, schemas.postList));
+        const endpoint = `${baseUrl}/communities/${communityId}/posts/?before=${now}&limit=${limit || ""}`;
+        return getCallApiAction(endpoint, schemas.postList);
     },
     loadNewerPosts: (communityId: number, afterDateTime: Date, limit = null) => {
         const now = new Date().toUTCString();
         const queryString = `before=${now}&after=${afterDateTime.toUTCString()}&limit=${limit || ""}`;
-        const endpoint = `${baseUrl}/communities/${communityId}/posts?${queryString}`;
-        return (dispatch) => dispatch(getCallApiAction(endpoint, schemas.postList));
+        const endpoint = `${baseUrl}/communities/${communityId}/posts/?${queryString}`;
+        return getCallApiAction(endpoint, schemas.postList);
     },
     loadOlderPosts: (communityId: number, beforeDateTime: Date, limit) => {
         const queryString = `before=${beforeDateTime.toUTCString()}&limit=${limit || ""}`;
-        const endpoint = `${baseUrl}/communities/${communityId}/posts?${queryString}`;
-        return (dispatch) => dispatch(getCallApiAction(endpoint, schemas.postList));
+        const endpoint = `${baseUrl}/communities/${communityId}/posts/?${queryString}`;
+        return getCallApiAction(endpoint, schemas.postList);
     },
 };
 
-function getCallApiAction(endpoint: string, responseSchema) {
+function getCallApiAction(endpoint: string, responseSchema, source = null) {
     return ({
         [CALL_API]: {
             endpoint,
             method: "GET",
             types: [
-                "REQUEST",
                 {
-                    meta: { schema: responseSchema },
+                    meta: {
+                        schema: responseSchema,
+                        ...(source && { source }),
+                    },
+                    type: "REQUEST",
+                },
+                {
+                    meta: {
+                        schema: responseSchema,
+                        ...(source && { source }),
+                    },
                     type: "SUCCESS",
                 },
                 {
                     meta: (action, state, res) => {
                         if (res) {
                             return {
+                                meta: {
+                                    schema: responseSchema,
+                                    ...(source && { source }),
+                                },
                                 status: res.status,
                                 statusText: res.statusText,
                             };
                         } else {
                             return {
+                                meta: {
+                                    schema: responseSchema,
+                                    ...(source && { source }),
+                                },
                                 status: "Network request failed",
                             };
                         }
