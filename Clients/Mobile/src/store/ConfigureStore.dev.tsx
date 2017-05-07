4a9a0e4eff540898dd8acc9e24c1aee0cd4cc112
Jon Kelling <jonkelling@Jons-MacBook-Air.local>
2017-05-07 00:10:25 -0500

    rename apiAuthMiddleware file

--- Clients/Mobile/src/store/ConfigureStore.dev.tsx
+++ Clients/Mobile/src/store/ConfigureStore.dev.tsx
@@ -11,7 +11,7 @@ import rootReducer from "../reducers/index";
 // import api from "../middleware/api";
 // import auth from "../middleware/auth";
 import schemas from "../schemas";
-import apiAuthMiddleware from "./middleware/apiAuthMiddleware2";
+import apiAuthMiddleware from "./middleware/apiAuthMiddleware";
 
 // const { apiMiddleware } = require("redux-api-middleware");
 
