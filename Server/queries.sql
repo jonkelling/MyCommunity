762dee893c61197f31ee0209234961b9a9d511aa
Jon Kelling <jonkelling@Jons-MacBook-Air.local>
2017-06-01 17:11:40 -0500

    query updates

--- Server/queries.sql
+++ Server/queries.sql
@@ -1,5 +1,8 @@
-update dbo.Posts set [AuthorId] = 4 where [Headline] like 'Test%';
+--update dbo.Posts set [AuthorId] = 4 where [Headline] like 'Test%';
 
 --delete from dbo.Posts where [Headline] like 'Test%' and [Headline] like '%8';
 
 --update dbo.Users set [CommunityId] = 2 where [Email] = 'jonkelling+appletest2@gmail.com';
+
+select * from dbo.Posts where [AuthorId] = 4;
+select * from dbo.Communities;
